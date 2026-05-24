7f99a7a1837065c7a033f618fae6b3fb2d6980b8
gmvinothkumar1998 <gmvinothkumar1998@gmail.com>
2026-05-24 11:22:40 +0000

    server error

--- middleware.ts
+++ middleware.ts
@@ -9,5 +9,5 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
