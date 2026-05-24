04059a21a98a5e94d265ebc258e4c8174baafcb5
gmvinothkumar1998 <gmvinothkumar1998@gmail.com>
2026-05-24 16:53:33 +0000

    serverless problem

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -72,11 +72,6 @@ export async function fetchUserPosts(userId: string) {
       model: Thread,
       populate: [
         {
-          path: "author",
-          model: User,
-          select: "name image id",
-        },
-        {
           path: "community",
           model: Community,
           select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
