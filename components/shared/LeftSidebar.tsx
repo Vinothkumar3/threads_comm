e25f61a81817bae577c44828d817590df2ea65b7
gmvinothkumar1998 <gmvinothkumar1998@gmail.com>
2026-05-25 22:33:54 +0000

    callback error resolve

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,14 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs/app-beta";
+import { usePathname } from "next/navigation";
+import { SignedIn, useAuth, useClerk } from "@clerk/nextjs";
 
 import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
-  const router = useRouter();
   const pathname = usePathname();
+  const { signOut } = useClerk();
 
   const { userId } = useAuth();
 
@@ -44,18 +44,19 @@ const LeftSidebar = () => {
 
       <div className='mt-10 px-6'>
         <SignedIn>
-          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
-            <div className='flex cursor-pointer gap-4 p-4'>
-              <Image
-                src='/assets/logout.svg'
-                alt='logout'
-                width={24}
-                height={24}
-              />
+          <div
+            className='flex cursor-pointer gap-4 p-4'
+            onClick={() => signOut({ redirectUrl: '/sign-in' })}
+          >
+            <Image
+              src='/assets/logout.svg'
+              alt='logout'
+              width={24}
+              height={24}
+            />
 
-              <p className='text-light-2 max-lg:hidden'>Logout</p>
-            </div>
-          </SignOutButton>
+            <p className='text-light-2 max-lg:hidden'>Logout</p>
+          </div>
         </SignedIn>
       </div>
     </section>
