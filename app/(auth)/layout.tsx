293ec4dab3e0feb60ef52e1e77436dd3a6fa4755
gmvinothkumar1998 <gmvinothkumar1998@gmail.com>
2026-05-26 14:31:56 +0000

    middleware public key

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         baseTheme: dark,
       }}
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/api/webhook/clerk",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/api/webhook/clerk",
+  "/api/uploadthing",
 ]);
 
 export default clerkMiddleware((auth, request) => {
