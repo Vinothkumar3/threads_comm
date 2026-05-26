4eeab0e8e4260b38eca228dcf5a8814f25227eec
gmvinothkumar1998 <gmvinothkumar1998@gmail.com>
2026-05-26 14:12:01 +0000

    middleware problem

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,6 @@
 // Resource: https://docs.uploadthing.com/nextjs/appdir#create-a-nextjs-api-route-using-the-filerouter
 // Copy paste (be careful with imports)
-
+export const dynamic = "force-dynamic";
 import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
